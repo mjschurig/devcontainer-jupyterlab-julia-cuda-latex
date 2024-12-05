a7158a02cb5ffdaa6b9e6afc5a0a676450279747
Max Schurig <mjschurig@gmail.com>
2024-12-05 16:35:47 +0100

    init

---
+++ .devcontainer/julia-init.jl
@@ -0,0 +1,10 @@
+using Pkg
+
+# Essentials
+
+Pkg.add("IJulia")
+Pkg.add("LanguageServer")
+
+# Additional 
+
+Pkg.add("CUDA")
---
+++ .devcontainer/python-init.sh
@@ -0,0 +1,7 @@
+pip install JLDracula
+pip install jupyterlab-latex
+sudo apt-get update
+sudo apt-get install texlive-xetex
+pip install ipympl
+pip install jupyterlab-lsp==5.1.0
+pip install jupyter-matlab-proxy
